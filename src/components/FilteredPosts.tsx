89b0c1925761e16e360fed47e4f4388271f24486
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-01-28 14:47:22 +0100

    💄   F02 Change the button for tags

--- src/components/FilteredPosts.tsx
+++ src/components/FilteredPosts.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
-import { Badge } from "./ui/badge";
+
 import { Post } from "@/lib/types";
+import { Button } from "./ui/button";
 
 type FilteredPostsProps = {
   posts: Post[];
@@ -14,21 +15,25 @@ export function FilteredPosts({ posts }: FilteredPostsProps) {
         posts.map((post) => (
           <div key={post.slug}>
             <Link href={`/posts/${post.slug}`} className="flex flex-col gap-3">
+              <span className="text-muted-foreground  ">
+                {new Date(post.date).toLocaleDateString("fr-FR")}
+              </span>
               <h3 className="text-balance text-2xl font-semibold lg:text-3xl">
                 {post.title}
               </h3>
               <p className="text-muted-foreground">
                 {post.description || "No description available"}
               </p>
-              <div className="mt-3 flex items-center gap-2 text-sm">
-                <span className="text-muted-foreground">
-                  on {new Date(post.date).toLocaleDateString("fr-FR")}
-                </span>
-                <div className="flex flex-wrap gap-1">
+              <div className="mt-3 flex items-center gap-2 ">
+                <div className="flex flex-wrap gap-3">
                   {post.tags.map((tag) => (
-                    <Badge key={tag} variant="outline">
+                    <Button
+                      className="rounded-full"
+                      variant="secondary"
+                      key={tag}
+                    >
                       {tag}
-                    </Badge>
+                    </Button>
                   ))}
                 </div>
               </div>
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,5 +1,4 @@
 export const navBurgerLinks = [
-  { href: "/blog", label: "Blog" },
   { href: "/about", label: "About" },
   { href: "/contact", label: "Contact Us" },
 ];
