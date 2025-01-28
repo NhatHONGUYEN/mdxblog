81762caf82b84912e49315edd1665723fca51e5e
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-01-28 20:13:54 +0100

    💄   F02 :change the baground

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import Header from "@/components/Header";
+import { Ripple } from "@/components/ui/ripple";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="max-w-6xl mx-auto relative h-full w-full bg-white ">
-        <div className="absolute h-full -z-10 w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
+      <body className="max-w-6xl mx-auto ">
+        <Ripple />
         <Header />
 
         {children}
