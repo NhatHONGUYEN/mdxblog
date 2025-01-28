55a828fc04c5d9a18538ab30eeaaa6fd5cf9eb35
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-01-28 11:16:54 +0100

    ✨ F02 : add layout for the blog/slug

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,6 +2,9 @@ import fs from "fs";
 import path from "path";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import matter from "gray-matter";
+import { ChevronLeft } from "lucide-react";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 
 export default async function Post({
   params,
@@ -16,13 +19,43 @@ export default async function Post({
   }
 
   const fileContent = fs.readFileSync(filePath, "utf8");
-  const { content } = matter(fileContent);
+  const { content, data } = matter(fileContent);
+
+  const { description, date } = data;
 
   return (
-    <div className="container px-4 py-32">
-      <article className="prose mx-auto lg:prose-xl">
-        <MDXRemote source={content} />
-      </article>
-    </div>
+    <section className="py-32">
+      <div className="container">
+        <div className="relative flex flex-col justify-between gap-10 lg:flex-row">
+          <aside className="top-10 mx-auto h-fit w-full max-w-[65ch] lg:sticky lg:max-w-96">
+            <Link
+              href={"/"}
+              className="mb-5 flex items-center gap-1 text-muted-foreground hover:text-primary"
+            >
+              <ChevronLeft className="h-full w-4" />
+              Return to home
+            </Link>
+            <h1 className="mb-5 text-balance text-3xl font-bold lg:text-4xl">
+              {description}
+            </h1>
+            <div className="flex gap-3">
+              <Avatar className="size-7 rounded-full">
+                <AvatarImage
+                  src="https://shadcnblocks.com/images/block/avatar-1.webp"
+                  alt="placeholder"
+                />
+              </Avatar>
+              <div>
+                <h2 className="font-semibold">John Doe</h2>
+                <p className="text-xs text-muted-foreground">{date}</p>
+              </div>
+            </div>
+          </aside>
+          <article className="prose lg:prose-xl">
+            <MDXRemote source={content} />
+          </article>
+        </div>
+      </div>
+    </section>
   );
 }
