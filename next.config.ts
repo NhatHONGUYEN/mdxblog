3729dbf274fb40c44f2c63d45ab5459499284616
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-01-29 12:39:34 +0100

    ✅   F03: update our next config for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import createMDX from "@next/mdx";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
   images: {
     remotePatterns: [
       {
-        protocol: "https",
+        protocol: "https" as const,
         hostname: "res.cloudinary.com", // Domaine autorisé
         port: "", // Facultatif (laisser vide pour le port par défaut)
         pathname: "/**", // Autoriser toutes les images à partir de ce domaine
